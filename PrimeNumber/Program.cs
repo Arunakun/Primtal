0c4c7258e5025b57bad48ee1ebcb8f62e33e88e9
Arunakun <krho79123@edu.ucl.dk>
2026-02-26 04:44:14 -0800

    Program oprettet for at finde ud af, om et prædefineret tal er et primtal eller ej.

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace PrimeNumber
+
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputTal = 1299721;
+            bool primFlag = ErPrimtal(inputTal);
+//          bool primFlag = ErPrimtalAdvanced(inputTal);
+
+
+            if (primFlag)
+            {
+                Console.WriteLine($"\n\n{inputTal} er et primtal");
+            }
+            else
+            {
+                Console.WriteLine($"{inputTal} er ikke et primtal");
+            }
+        }
+
+
+        static bool ErPrimtal(int tal)
+        {
+            if (tal <= 1)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 2; i < tal; i++)
+                {
+                    if (tal % i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+
+
+        static bool ErPrimtalAdvanced(int tal)
+        {
+            if (tal <= 1)
+            {
+                return false;
+            }
+
+            else if (tal <= 3)
+            {
+                return true;
+            }
+
+            else if (tal % 2 == 0 || tal % 3 == 0)
+            {
+                return false;
+            }
+
+            int i = 5;
+            while (i * i <= tal)
+            {
+                if (tal % i == 0 || tal % (i + 2) == 0)
+                {
+                    return false;
+                }
+                i += 6;
+            }
+            return true;
+        }
+    }
+}
